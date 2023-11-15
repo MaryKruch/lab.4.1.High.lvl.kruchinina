98b6de48773952532cbbe40a5e8587d756feca7c
kbk.local <kbk.local@DESKTOP-N6VKFM1>
2023-11-15 17:37:34 +0200

    Добавьте файлы проекта.

---
+++ lab.4.1.High.lvl.kruchinina/Program.cs
@@ -0,0 +1,28 @@
+﻿//22var high lvl
+Console.Write("Введите размер массива:");
+int s = int.Parse(Console.ReadLine());
+int[] mas = new int[s];
+Random r = new Random();
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = r.Next(10, 100);
+    Console.Write(mas[i] + " ");
+}
+Console.WriteLine();
+
+for (int i = 1; i < mas.Length - 1; i++)
+{
+    if (mas[i] > mas[i - 1] && mas[i] < mas[i + 1])
+    {
+        s = i;
+    }
+}
+
+if (s != -1)
+{
+    Console.WriteLine("Номер последнего элемента: " + s);
+}
+else
+{
+    Console.WriteLine("Таких элементов нет");
+}
